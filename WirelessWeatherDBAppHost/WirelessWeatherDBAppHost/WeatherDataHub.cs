735d0325e36b0bb05502346a52cedb950c8bb2f8
Dagim Feyessada <dagimf@hotmail.com>
2014-05-20 12:21:35 +0300

    before
    checkpoint

---
+++ WirelessWeatherDBAppHost/WirelessWeatherDBAppHost/WeatherDataHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using WirelessWeatherDB.DataModel.Models;
+
+namespace WirelessWeatherDBAppHost
+{
+    
+    public class WeatherDataHub : Hub
+    {
+        public void UpdateData(WeatherReading updatedData)
+        {
+            Clients.All.updateData(updatedData);
+        }
+    }
+}
